967e0e0e4a3e8fb7732b57118bfeb8f8d8253259
canesche <michael.canesche@gmail.com>
2018-12-01 11:31:26 -0200

    add model to python in repository Algorithm

---
+++ python/__model__/README.md
@@ -0,0 +1,8 @@
+# Speech Recognition 
+
+This is a simple model to my git.
+
+## To install dependences
+```console
+pip3 install -U -r requirements.txt
+``` 
---
+++ python/__model__/main.py
@@ -0,0 +1,14 @@
+'''
+    Author: M. Canesche
+    Date: xx/xx/xxxx
+    About: This is a model to documentation on git.
+'''
+
+# libraries
+
+
+def main():
+    pass
+
+if __name__ == "__main__" :
+    main()
